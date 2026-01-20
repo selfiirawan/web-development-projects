e510a6d4234f8c20969ad4f8be5b74060694ac8b
Selfi <celpi.irawan@gmail.com>
2026-01-20 19:21:30 +0800

    [PROJECT] Dice Game

---
+++ JavaScript/Dicee+Challenge+-+Starting+Files/index.js
@@ -0,0 +1,16 @@
+const randomNumber1 = Math.floor(Math.random() * 6) + 1;
+const randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+const image1 = "./images/dice" + randomNumber1 + ".png";
+const image2 = "./images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".img1").setAttribute("src", image1);
+document.querySelector(".img2").setAttribute("src", image2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+    document.querySelector("h1").innerHTML = "It's a Draw!";
+}
