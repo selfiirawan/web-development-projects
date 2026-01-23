ac6863c16b8701db40a801ad57272de55b0430cf
Selfi <celpi.irawan@gmail.com>
2026-01-23 19:33:42 +0800

    add animation on keydown

--- JavaScript/Drum Kit Starting Files/index.js
+++ JavaScript/Drum Kit Starting Files/index.js
@@ -6,11 +6,13 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
         var buttonInnerHTML = this.innerHTML;   
 
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
     });
 
     // Detect Pressed Keyboard
     document.addEventListener("keydown", function(event) {
         makeSound(event.key);
+        buttonAnimation(event.key);
     });
 }
 
@@ -54,4 +56,13 @@ function makeSound(key) {
     
         default: console.log(buttonInnerHTML);
     } 
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
