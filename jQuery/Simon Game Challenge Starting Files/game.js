a16125218dc0422d90054a0286c14a0530ff7d67
Selfi <celpi.irawan@gmail.com>
2026-01-31 18:56:25 +0800

    [PROJECT] Simon Game

--- jQuery/Simon Game Challenge Starting Files/game.js
+++ jQuery/Simon Game Challenge Starting Files/game.js
@@ -18,13 +18,29 @@ $(".btn").click(function() {
     userClickedPattern.push(userChosenColor);
 
     playSound(userChosenColor);
+    animatePress(userChosenColor);
+
+    checkAnswer(userClickedPattern.length-1);
 })
 
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("success");
+
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+    }
+}
 
 function nextSequence() {
+    userClickedPattern = [];
+
     level++;
     $("#level-title").text("Level " + level);
-    
+
     const randomNumber = Math.floor(Math.random() * 4);
     
     const randomChosenColor = buttonColors[randomNumber];
@@ -47,3 +63,4 @@ function animatePress(currentColor) {
         $("#" + currentColor).removeClass("pressed");
     }, 100);
 }
+
