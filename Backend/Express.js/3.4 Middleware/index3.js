f4984a570a86403e5ee73c03e08fcd9ab37b7291
Selfi <celpi.irawan@gmail.com>
2026-02-17 15:37:14 +0800

    custom middleware

--- Backend/Express.js/3.4 Middleware/index3.js
+++ Backend/Express.js/3.4 Middleware/index3.js
@@ -3,6 +3,12 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// custom logger middleware function
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+}
+
 app.use(logger);
 
 app.get("/", (req, res) => {
