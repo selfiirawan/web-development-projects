8dd63e223db58e9c6ac913cb00235a595fbad1b1
Selfi <celpi.irawan@gmail.com>
2026-02-18 16:28:19 +0800

    edit code line 31

--- Backend/Express.js/3.5 Secrets Project/index.js
+++ Backend/Express.js/3.5 Secrets Project/index.js
@@ -28,7 +28,7 @@ app.post("/check", (req, res) => {
     if (userIsAuthorised) {
         res.sendFile(__dirname + "/public/secret.html");
     } else {
-        res.sendFile(__dirname + "/public/index.html");
+        res.redirect("/");
     }
 })
 
