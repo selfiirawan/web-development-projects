b6bc7948e7d261f9f391953b1404938ac1a3ab0b
selfiirawan <celpi.irawan@gmail.com>
2026-05-10 15:24:58 +0800

    build API for PUT

--- Backend/DIY_API/6.0 DIY API/index.js
+++ Backend/DIY_API/6.0 DIY API/index.js
@@ -49,6 +49,19 @@ app.post("/jokes", (req, res) => {
 });
 
 //5. PUT a joke
+app.put("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const editedJoke = {
+    id: id,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  };
+
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+
+  jokes[searchIndex] = editedJoke;
+  res.json(editedJoke);
+});
 
 //6. PATCH a joke
 
