3e1d2226c10d08569efcf72d1d62b05907f004c3
selfiirawan <celpi.irawan@gmail.com>
2026-05-31 19:31:10 +0800

    exercise on postgres

--- Database/Postgres/World+Capital+Quiz/index.js
+++ Database/Postgres/World+Capital+Quiz/index.js
@@ -1,14 +1,33 @@
 import express from "express";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import pg from "pg";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
-let quiz = [
-  { country: "France", capital: "Paris" },
-  { country: "United Kingdom", capital: "London" },
-  { country: "United States of America", capital: "New York" },
-];
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: process.env.PG_PASSWORD,
+  port: 5432,
+});
+
+db.connect();
+
+let quiz = [];
+db.query("SELECT * FROM capitals", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    quiz = res.rows;
+  }
+  
+  db.end();
+});
 
 let totalCorrect = 0;
 
