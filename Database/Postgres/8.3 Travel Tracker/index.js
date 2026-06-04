04749844cf3ca766de321f3426097d3fb7e38951
selfiirawan <celpi.irawan@gmail.com>
2026-06-04 20:46:21 +0800

    Travel Tracker: last solution [DONE]

--- Database/Postgres/8.3 Travel Tracker/index.js
+++ Database/Postgres/8.3 Travel Tracker/index.js
@@ -43,8 +43,8 @@ app.post("/add", async (req, res) => {
 
   try {
     const result = await db.query(
-      "SELECT country_code FROM countries WHERE LOWER(country_name) = LOWER($1)",
-      [input]
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+      [input.toLowerCase()]
     );
 
     const data = result.rows[0];
