3b904da070bb6c32e5fdf7240eef27632fe45e70
selfiirawan <celpi.irawan@gmail.com>
2026-06-04 19:15:31 +0800

    travel tracker part 2: add app.post route

--- Database/Postgres/8.3 Travel Tracker/index.js
+++ Database/Postgres/8.3 Travel Tracker/index.js
@@ -27,9 +27,31 @@ app.get("/", async (req, res) => {
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
-  console.log(result.rows);
+  console.log("result.rows: ", result.rows);
   res.render("index.ejs", { countries: countries, total: countries.length });
   // db.end();
+});
+
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1",
+    [input]
+  );
+  console.log("Result: ", result.rows);
+
+  if (result.rows.length !== 0) {
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+
+    await db.query(
+      "INSERT INTO visited_countries (country_code) VALUES ($1)",
+      [countryCode]
+    );
+
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
